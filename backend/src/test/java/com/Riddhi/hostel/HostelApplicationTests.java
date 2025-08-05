f30f8cc3faddb5a6313f5dcefa1e3467ac53a69c
Riddhi Shintre <pratikshashintre@gmail.com>
2025-08-05 22:13:17 +0530

    Initial commit - Spring Boot backend

---
+++ backend/src/test/java/com/Riddhi/hostel/HostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Riddhi.hostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
