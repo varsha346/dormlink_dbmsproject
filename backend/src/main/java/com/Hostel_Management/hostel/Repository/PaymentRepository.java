fe4ed036e1890592ddc74f7bfb5e90a6e253ba2a
varsha tembugade <varshatembugade346@gmail.com>
2025-10-29 23:01:54 +0530

    Change PaymentRepository to use Payment model

--- backend/src/main/java/com/Hostel_Management/hostel/Repository/PaymentRepository.java
+++ backend/src/main/java/com/Hostel_Management/hostel/Repository/PaymentRepository.java
@@ -1,8 +1,8 @@
 package com.Hostel_Management.hostel.Repository;
 
-import com.Hostel_Management.hostel.models.Room;
+import com.Hostel_Management.hostel.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<Room.Payment, Long> {
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
 }
